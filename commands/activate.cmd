ffd63e5bf97fc014b6fd83ba619679b7b3348932
zalt00 <leberre.amedee@gmail.com>
2021-04-04 22:40:45 +0200

    correction de bug

--- commands/activate.cmd
+++ commands/activate.cmd
@@ -2,8 +2,28 @@
 
 echo --------- Environment being activated ---------
 
-set "path2scripts=%~dp0\..\venv\Scripts"
 set "BASE=%~dp0\.."
-set "PATH=%~dp0;%path2scripts%;%PATH%"
 set "PYTHONPATH=%base%;%base%\sources;%PYTHONPATH%"
+
+
+if exist "%~dp0\..\venv" (goto activate_venv) else (goto activate_portable_embedded_python)
+
+:activate_portable_embedded_python:
+echo info - activating portable embedded python
+set "path2interpreterdir=%~dp0\..\python"
+set "PATH=%~dp0;%path2interpreterdir%;%PATH%"
+goto end
+
+
+:activate_venv:
+echo info - activating virtual environnement
+set "path2scripts=%~dp0\..\venv\Scripts"
+set "PATH=%~dp0;%path2scripts%;%PATH%"
 call "%base%\venv\Scripts\activate.bat"
+goto end
+
+
+:end:
+
+
+
